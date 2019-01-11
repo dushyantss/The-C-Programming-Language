b53845283092d8e1deb5ba3777c7f25948720283
Dushyant Singh Shekhawat <dushyant225@gmail.com>
2019-01-11 19:59:58 +0530

    Finish 1-23.

---
+++ exercises/chapter 1/1-23/1-23.c
@@ -0,0 +1,28 @@
+/* Write a program to remove all comments from a C program. Don't forget to
+ * handle quoted strings and character constants properly. C comments don't
+ * nest.
+ */
+
+#include <stdio.h>
+
+int remove_comment(void);
+
+int main(void)
+{
+	int c;
+	int last;
+
+	last = EOF;
+	while ((c = getchar()) != EOF) {
+		if (last == '/' && c == '*') {
+			while ((c = getchar()) != EOF && !(last == '*' && c == '/'))
+				last = c;
+			last = EOF;
+		} else {
+			if (last != EOF)
+				putchar(last);
+
+			last = c;
+		}
+	}
+}
