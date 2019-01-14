fb09c7e6449b17fa123e8cc8597d044a022bf421
Dushyant Singh Shekhawat <dushyant225@gmail.com>
2019-01-14 22:13:49 +0530

    Finish 2-10.

---
+++ exercises/chapter 2/2-10/2-10.c
@@ -0,0 +1,21 @@
+/* Rewrite the function lower, which converts upper case letters to lower case,
+ * with a conditional expression instead of if-else.
+ */
+
+#include <stdio.h>
+
+int lower(int c);
+
+int main(void)
+{
+	int c;
+	while ((c = getchar()) != EOF)
+		putchar(lower(c));
+	return 0;
+}
+
+/* lower:  convert c to lower case; ASCII only */
+int lower(int c)
+{
+	return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
