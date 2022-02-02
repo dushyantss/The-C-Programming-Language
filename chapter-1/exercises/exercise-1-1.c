1466df46e3bbfb90ddc2839b20cf8e9da8c7d05e
Dushyant Singh Shekhawat <contact@dushyantss.com>
2022-02-02 12:28:10 +0530

    Restart the book

--- chapter-1/exercises/exercise-1-1.c
+++
@@ -1,8 +0,0 @@
-/* Run the ``hello, world'' program on your system. Experiment with leaving out parts of the program, to see what error messages you get. */
-
-#include <stdio.h>
-
-int main()
-{
-	printf("hello, world\n");
-}
--- chapter-1/exercises/exercise-1-2.c
+++
@@ -1,8 +0,0 @@
-/* Experiment to find out what happens when prints's argument string contains \c, where c is some character not listed above. */
-
-#include <stdio.h>
-
-int main()
-{
-	printf("hello, world\d");
-}
